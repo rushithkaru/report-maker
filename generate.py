cdfe006beba9874d7db858759eb839a373dc11c9
Rushith Nalin Vincent Karunaratne <rukarunaratne@gmail.com>
2023-03-09 23:06:41 +1100

    Functionality added

--- generate.py
+++ generate.py
@@ -1,24 +1,35 @@
 """
-Functionality (not implemented): Get sensor data from database and generate report
+WIP:
+Functionality (planned): Get sensor data from database and generate report
 with graphs.
-
+So far only creates one graph. 
+Plan is to integrate with this repo: "https://github.com/rushithkaru/Aws_telemetry"
 """
 
 from fpdf import FPDF
 import psycopg2
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
+import datetime as dt
+import creds
 print(os.environ.get("timedb"))
 
-def get_data():
+def get_data(device,start,end):
     
 
     df = pd.DataFrame(columns=['DateTime','sensor_id','temp','cpu'])
+    CONNECTION = creds.con_string
 
     with psycopg2.connect(CONNECTION) as conn:
             cursor = conn.cursor()
-            query = "SELECT * FROM sensor_data WHERE sensor_id = 3;"
+            start = dt.date(2023,3,7) 
+            print(start.__class__)
+            next = start + dt.date.resolution
+            query = "SELECT * FROM sensor_data WHERE time > '%s/%s/%s' AND sensor_id = %s;" % (start.year,start.month,start.day,str(device))
+            
+            print(query)
             cursor.execute(query)
             count = 0
             tableData = cursor.fetchall()
@@ -28,34 +39,36 @@ def get_data():
 
             df = pd.DataFrame.from_records(data)
             cursor.close()
-    print(pd.to_datetime(df['DateTime'][0]))
-    df['date'] = pd.to_datetime(df['DateTime'])
 
-    fig = plt.figure()
-    # Plot the temperature data
-    plt.plot(df['date'], df['temp'])
-
-    # Set the x-axis label and format
-    plt.xlabel('Date')
-    plt.xticks(rotation=45, ha='right')
-
-    # Set the y-axis label
-    plt.ylabel('Temperature')
-
-    # Set the title of the plot
-    plt.title('Temperature Data')
-    plt.savefig('graphs/trial.png')
-    #plt.show()
+    return df
     
+
+def create_graphs(device,start,end):
+      
+        df = get_data(device,start,end)
+        print(pd.to_datetime(df['DateTime'][0]))
+    
+        fig, ax = plt.subplots(figsize = (15, 7))
+
+        sns.lineplot(ax = ax, x='DateTime', y='temp', data=df).set_title('Title')
+
+        plt.xlabel('DateTime')
+        plt.ylabel('Magnitude (Mw)')
+        #plt.show()       
+        plt.savefig('graphs/trial2.png')
+        create_report()
+
 
 def create_report():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('helvetica', size=12)
     pdf.cell(txt="hello world")
-    pdf.image('graphs/trial.png', x=50, y=100, w=50, h=50)
+    pdf.image('graphs/trial2.png', x=50, y=100, w=50, h=50)
     pdf.output("reports/hello_world.pdf")
 
 if __name__ == "__main__":
-    get_data()
-    create_report()
+    
+    start = dt.date(2023,3,7) 
+    end = dt.date(2023,3,10)
+    create_graphs(2,start,end)
